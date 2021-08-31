3e44a140a9c324c5f2f95005b8eca0b6fa329984
fabian winter <fabianwinter93@gmail.com>
2021-08-31 16:13:10 +0200

    first work in main

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from argparse import ArgumentParser
+#from argparse import ArgumentParser
 import glob
 import os
 
-import data
-import voting_experts
+import random
 
+from pprint import pprint
+
+from config import *
+from data import * 
+from voting_experts import *
+
+if __name__ == "__main__":
+
+    files = glob.glob("data/grimm/*/*.txt")
+    files = [os.path.normcase(f) for f in files]
+
+    n_files = len(files)
+
+    
+    
+    trie = build_trie(process(files[0]), 7)
+    
+    trie.print()
